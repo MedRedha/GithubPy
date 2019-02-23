2b3f0f7a2c982702f374eef8a64142bcce68f57e
Ishan Dutta <ishandutta2007@MacBook-Pro.local>
2019-02-23 21:59:24 +0530

    first commit

---
+++ githubpy/__init__.py
@@ -0,0 +1,7 @@
+
+from .githubpy import GithubPy
+from .githubpy import smart_run
+from .settings import Settings
+
+# __variables__ with double-quoted values will be available in setup.py
+__version__ = "0.1.3"
